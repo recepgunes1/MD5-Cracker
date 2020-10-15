01e0566d1690384e17846dada925229a1967f64f
Rahulmehta123 <41620728+Rahulmehta123@users.noreply.github.com>
2020-10-15 22:15:02 +0530

    Update md5-cracker.py

--- md5-cracker.py
+++ md5-cracker.py
@@ -4,6 +4,9 @@ __author__ = 'Recep Gunes'
 
 import requests, sys, time
 from bs4 import BeautifulSoup
+import hashlib
+from termcolor import colored
+
 
 def banner():
     print("""{}
@@ -104,3 +107,68 @@ if __name__ == '__main__':
         print("Please, Entry A Hash Text!")
     except:
         print("Don't Know Error!")
+        
+        
+        
+        
+        
+hashvalue = input("[*] Enter Your String: ")
+type = input("[*] Enter Your Hash Type: ")
+
+hash1 = 'md5'
+hash2 = 'sha1'
+hash3 = 'sha256'
+hash4 = 'sha384'
+hash5 = 'sha512'
+hash6 = 'sha224'
+hash7 = 'blake2c'
+hash8 = 'blake2b'
+
+
+if type == hash1:
+	hashobj = hashlib.md5()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash2:
+	hashobj = hashlib.sha1()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash3:
+	hashobj = hashlib.sha256()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash4:
+	hashobj = hashlib.sha384()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash5:
+	hashobj = hashlib.sha512()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash6:
+	hashobj = hashlib.sha224()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash7:
+	hashobj = hashlib.blake2s()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+elif type == hash8:
+	hashobj = hashlib.blake2b()
+	hashobj.update(hashvalue.encode())
+	print (hashobj.hexdigest())
+	quit()
+
+
+
+print ("[!!] Please Enter Valid Hash  Type....!!!!!!!!")
+
+        
+        
